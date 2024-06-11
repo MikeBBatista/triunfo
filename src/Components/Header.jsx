90956ff8376a95e8391028c1f91f71c772fe9d11
Mike <mikebbatista@hotmail.com>
2024-06-11 15:44:04 -0300

    insercao do sobre nos na pagina home

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Header.module.css';
 import logo from '../assets/logo.svg'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const [show, setShow] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    
+    if (window.scrollY > lastScrollY) { // if scroll down hide the navbar
+      setShow(true); 
+      console.log('teste');
+    } else { // if scroll up show the navbar
+      setShow(false);  
+    }
+
+    // remember current page location to use in the next move
+    setLastScrollY(window.scrollY); 
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', controlNavbar);
+
+    // cleanup function
+    return () => {
+       window.removeEventListener('scroll', controlNavbar);
+    };
+  }, [lastScrollY]);
   return (
-    <div>
+    <div className={`active ${show && styles.hidden}`}>
         <nav>
             <div className={styles.navDiv}>
                 <Link to="/" aria-label='Triunfo - Home'>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,6 +3,7 @@ import styles from './Home.module.css'
 import main_image from '../assets/first_image.png'
 import led from '../assets/led.png'
 import { Link } from 'react-router-dom';
+import YoutubePlayer from "./YoutubePlayer";
 
 const Home = () => {
   return (
@@ -19,17 +20,39 @@ const Home = () => {
       </div>
       <div className={styles.secondBlock}>
         <div className={styles.aboutUs}>
+          <div className={styles.aboutUsTitle}>
           <h2>SOBRE NÓS</h2>
+          </div>
+          <div className={styles.divImgAboutUs}>
           <img src={led} className={styles.aboutUsImg} />
+          </div>
         </div>
         <p className={styles.secondBlockP}>
           Nossa missão é desenvolver jogos que coloquem a arte no centro da experiência, combinando visuais deslumbrantes, 
           narrativas envolventes e jogabilidade inovadora.
         </p>
-        <Link to="/aboutUs"><button className={styles.btn}>CONHEÇA NOSSA HISTÓRIA</button></Link>
+        <Link to="/aboutUs">
+        <div className={styles.btnAboutUs}>
+          <button className={styles.btn}>CONHEÇA NOSSA HISTÓRIA</button>
+        </div>
+        </Link>
       </div>
       <div className={styles.thirdBlock}>
         <h2>NOSSO PROJETO</h2>
+        <p className={styles.secondBlockP}>Em nosso primeiro projeto, <span className={styles.specialWorldRed}>SI</span><span className={styles.specialWorldBlue}>NA</span>, embarcamos em uma aventura épica ambientada em um universo [Gênero do Jogo] onde a arte é a força vital que molda o mundo. Os jogadores assumem o papel de [Protagonista], um [Descrição do Protagonista] que deve usar seus talentos artísticos para [Objetivo do Jogador].</p>
+        <div className={styles.divVideoPlayer}>
+          <div className={styles.videoPlayer}>
+            <YoutubePlayer embedId="mdwX3ac1sfo" />
+          </div>
+          <div className={styles.divBtnOurProject}>
+            <button className={styles.btn}>BAIXE O DEMO NA STEAM</button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.thirdBlock}>
+      <div className={styles.aboutUsTitle}>
+          <h2>SLIDE SHOW</h2>
+          </div>
       </div>
     </div>
   )
