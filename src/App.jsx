6f76b8dc211f5adfe1dbf18691f3784affaebedd
Mike <mikebbatista@hotmail.com>
2024-06-25 21:37:46 -0300

    importando a font manualmente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import './fonts/fonts.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Components/Home';
